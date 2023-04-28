7a4e51afa400a0dab5bda1f217e2620c09c51b2d
assleeyesjorje <90770770+assleeyesjorje@users.noreply.github.com>
2023-04-28 16:04:52 +0100

    Update login.php

--- login.php
+++ login.php
@@ -17,24 +17,24 @@ include("includes/connection.php");
 
          if(password_verify($pass, $hashed_password)){
             echo "<script>alert('Password Correct')</script>"; //alert to confirm hash verified
-        
-        $select_block_users = "select * from users2 where user_email='$email' and user_pass='$hashed_password' and block = 1";
-        $block_query = mysqli_query($con, $select_block_users);
-        $check_block_user = mysqli_num_rows($block_query);
+               
+    }
+    $select_block_users = "select * from users2 where user_email='$email' and user_pass='$hashed_password' and block = 1";
+    $block_query = mysqli_query($con, $select_block_users);
+    $check_block_user = mysqli_num_rows($block_query);
 
-        $select_users = "select * from users2 where user_email='$email' and user_pass='$hashed_password' and status='verified'";
-        $query = mysqli_query($con, $select_users);
-        $check_user = mysqli_num_rows($query);
-        
+    $select_users = "select * from users2 where user_email='$email' and user_pass='$hashed_password' and status='verified'";
+    $query = mysqli_query($con, $select_users);
+    $check_user = mysqli_num_rows($query);
+    
 
-        if($check_block_user == 1) {
-           echo "<script>alert('You have been blocked by admin')</script>";  
-        }
-       
-        elseif( $check_user == 1) {
-            $_SESSION['user_email'] = $email;
-            echo "<script>window.open('home.php', '_self')</script>";
-        }
+    if($check_block_user == 1) {
+       echo "<script>alert('You have been blocked by admin')</script>";  
+    }
+   
+    elseif( $check_user == 1) {
+        $_SESSION['user_email'] = $email;
+        echo "<script>window.open('home.php', '_self')</script>";
     }
         else {
             echo "<script>alert('Your Email or Password is incorrect')</script>";
