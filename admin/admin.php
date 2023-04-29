ff3ab29bae21c87b78ff7d1b9a62b184423e4dc4
Benhur <x22168494@student.ncirl.ie>
2023-04-29 14:04:41 +0100

    Cometics on admin page

--- admin/admin.php
+++ admin/admin.php
@@ -4,6 +4,84 @@
 	<title>Admin Page</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+	<style>
+		* {
+			box-sizing: border-box;
+			margin: 0;
+			padding: 0;
+			font-family: Arial, sans-serif;
+		}
+		
+		body {
+			background-color: #f2f2f2;
+		}
+		
+		header {
+			background-color: #4967b8;
+			color: #fff;
+			padding: 20px;
+			text-align: center;
+		}
+		
+		nav {
+			background-color: #f2f2f2;
+			border-bottom: 1px solid #ccc;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			padding: 10px;
+		}
+		
+		nav ul {
+			display: flex;
+			list-style: none;
+		}
+		
+		nav ul li a {
+			color: #333;
+			display: block;
+			padding: 10px;
+			text-decoration: none;
+			transition: color 0.3s ease;
+		}
+		
+		nav ul li a:hover {
+			color: #666;
+		}
+		
+		nav ul li.active a {
+			color: #666;
+			font-weight: bold;
+		}
+		
+		main {
+			padding: 20px;
+		}
+		
+		footer {
+			background-color: #4967b8;
+			color: #fff;
+			padding: 10px;
+			text-align: center;
+		}
+		
+		@media only screen and (max-width: 768px) {
+			nav {
+				flex-direction: column;
+				align-items: flex-start;
+			}
+			
+			nav ul {
+				flex-direction: column;
+				width: 100%;
+			}
+			
+			nav ul li {
+				width: 100%;
+			}
+		}
+	</style>
 </head>
 <body>
 	<header>
@@ -11,15 +89,19 @@
 	</header>
 	<nav>
 		<ul>
-			<li><a href="#">Dashboard</a></li>
-			<li><a href="/users_admin.php">Manage Users</a></li>
-           
-			<li><a href="#">Settings</a></li>
-			<li><a href="/logout.php">Logout</a></li>
+			<li><a href="#" class="active"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
+			<li><a href="/users_admin.php"><i class="fas fa-users"></i> Manage Users</a></li>
+			<li><a href="#"><i class="fas fa-cog"></i> Settings</a></li>
+			<li><a href="/logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
 		</ul>
+		<form action="#" method="get">
+			<input type="text" placeholder="Search...">
+			<button type="submit"><i class="fas fa-search"></i></button>
+		</form>
 	</nav>
 	<main>
-		<p>Welcome to the admin page. Here you can manage the website and its content.</p>
+		<h2>Welcome to the Admin Page</h2>
+		<p>Here you can manage the website and its content.</p>
 	</main>
 	<footer>
 		<p>&copy; 2023 Admin Page</p>
